c4e20d9eba715aa397a02b799e3a08697a9f7767
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-02-25 10:57:25 -0700

    This fork is looking at a socket with longing eyes.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,21 @@
 class Owner
   attr_reader :name,
-              :occupation
-              
+              :occupation,
+              :cars
+
   def initialize(name, occupation)
     @name = name
     @occupation = occupation
+    @cars = []
   end
+
+  def buy(car_info)
+      make = car_info.split[2]
+      model = car_info.split[3]
+      color = car_info.split[1]
+      year = car_info.split[0]
+      new_car = {:description => color + " " + " " + make + " " + model,
+      :year => year}
+      @cars << Car.new(new_car)
+    end
 end
--- test/owner_test.rb
+++ test/owner_test.rb
@@ -6,6 +6,7 @@ require './lib/owner'
 class OwnerTest < Minitest::Test
   def setup
     @owner_1 = Owner.new('Regina George', 'Heiress')
+    @car_1 = Car.new({description: 'Green Ford Mustang', year: '1967'})
   end
 
   def test_it_exists
@@ -15,5 +16,14 @@ class OwnerTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Regina George", @owner_1.name
     assert_equal "Heiress", @owner_1.occupation
+  end
+
+  def test_it_starts_with_no_cars
+    assert_equal [], @owner_1.cars
+  end
+
+  def test_it_can_buy_cars
+    @owner_1.buy('1967 Green Ford Mustang')
+    assert_equal [@car_1], @owner_1.cars
   end
 end
