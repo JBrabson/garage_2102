f6b2568f38bdc4c13e5efe1790c5f33bbc46529d
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-02-25 09:19:32 -0700

    Refactor initialize for correct return. Code @age to allow for any year; attributes tests passing.

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,16 @@
 class Car
+  attr_reader :make,
+              :model,
+              :color,
+              :year,
+              :age
 
   def initialize(details)
+    @make = details[:description].split[1]
+    @model = details[:description].split[2]
+    @color = details[:description].split[0]
+    @year = details[:year]
+    @age = Time.now.year - (details[:year]).to_i
   end
+
 end
